dd96a0f380f4e8da5c3978f6c0292c8403a5e5ef
0xzolty <146833060+0xzolty@users.noreply.github.com>
2026-04-28 21:12:39 +0200

    Add script to automate data entry from text files
    This script allows users to select a text file, read its contents, and automate data entry into Excel using keyboard shortcuts.

---
+++ FromFile.py
@@ -0,0 +1,46 @@
+import keyboard  # pip install keyboard
+import os
+import pyperclip # pip install pyperclip
+import time 
+
+while True:
+    
+    
+    file_txt = [f for f in os.listdir() if f.endswith('.txt')]
+    print("\n Wszystkie pliki  w folderze : ")
+
+
+    for i, plik in enumerate(file_txt, start=1):
+        print(f"  {i}. {plik}")
+    
+    numer = int(input("\nWybierz numer pliku: "))
+    userinput = file_txt[numer - 1]  
+
+
+    with open(userinput, 'r') as f:
+        strings = f.read().split(',')
+    print(strings) 
+
+    print("\n Kliknij na 1 atrybut kolumny i kliknij f1 żeby zacząć wpisywać dane")
+    keyboard.wait('f1')
+
+    
+    for string in strings:
+        keyboard.press_and_release('f2')
+        time.sleep(0.05)
+        pyperclip.copy(string) 
+        time.sleep(0.1)           
+        keyboard.press_and_release('ctrl+v')
+        time.sleep(0.05)
+        keyboard.press_and_release('enter')
+        time.sleep(0.05)
+
+    print("Wszystkie dane z pliku wpisane w excelu")
+
+   
+
+
+    
+    choice = input("\n Wciśnij n aby wypisac kolejny plik, q aby zakończyć: ").lower()
+    if choice == 'q':
+        break
